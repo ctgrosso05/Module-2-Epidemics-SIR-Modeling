9dbcf60071aeba970cfa1e1baa94e200dadc9cb7
ctgrosso05 <ctgrosso05@gmail.com>
2026-02-26 12:28:52 -0500

    Update

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -1,10 +1,19 @@
-#%%
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#%%
-# Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+# Get the directory where this script lives
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-#%%
-# Make a plot of the active cases over time
+# Build path to Data folder
+data_path = os.path.join(script_dir, "..", "Data",
+                         "mystery_virus_daily_active_counts_RELEASE#1.csv")
+
+data = pd.read_csv(data_path, parse_dates=['date'])
+
+plt.figure()
+plt.plot(data["day"], data["active reported daily cases"])
+plt.xlabel("Day")
+plt.ylabel("Active Infections")
+plt.title("Day vs Active Infections (Data Release #1)")
+plt.show()
